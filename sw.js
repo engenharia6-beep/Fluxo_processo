f1cc57fd581f35d863ded70a111ac7fc095b170c
engenharia6-beep <engenharia6@futuraeletronicos.com.br>
2026-06-05 10:13:32 -0400

    Replace icon-192.png with icon-512.png in cache

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const ASSETS = [
   './Futura.png',
   './manifest.json',
   './icon-192.png',
-  './icon-192.png',
+  './icon-512.png',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap'
 ];
 
